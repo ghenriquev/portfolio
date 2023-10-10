575d3294955779c6fdafe056274437338e25c166
Gabriel Henrique <gabriel.emv@hotmail.com>
2023-10-10 20:33:45 -0300

    desfazendo erros

--- js/script.js
+++ js/script.js
@@ -58,3 +58,17 @@ const aboutDescription = document.querySelector(".about-description");
 const aboutTechs = document.querySelector('.about-techs');
 
 const aboutObserver = new IntersectionObserver((entries) => {});
+
+const cardImage = document.querySelector('.card-img');
+
+cardImage.addEventListener('mouseenter', (e) => {
+  const cardOverlay = e.target.children[0];
+  cardOverlay.classList.remove('hidden');
+  cardOverlay.classList.add('flex');
+});
+
+cardImage.addEventListener('mouseleave', (e) => {
+  const cardOverlay = e.target.children[0];
+  cardOverlay.classList.remove('flex');
+  cardOverlay.classList.add('hidden');
+})
